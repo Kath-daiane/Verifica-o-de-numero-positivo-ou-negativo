d1b5194feb706c1d652186d16ce2d667345f64ac
Kath-daiane <kath.daiane.p@gmail.com>
2025-02-27 10:45:52 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Pedir ao usuário para digitar um número
+        Console.Write("Digite um número: ");
+        int numero = Convert.ToInt32(Console.ReadLine());
+
+        // Verificar se o número é positivo, negativo ou zero
+        if (numero > 0)
+        {
+            Console.WriteLine("O número é positivo.");
+        }
+        else if (numero < 0)
+        {
+            Console.WriteLine("O número é negativo.");
+        }
+        else
+        {
+            Console.WriteLine("O número é zero.");
+        }
+    }
+}
